357b5e07f2773b534ef8fdffc0eb2679bbe6eac0
Wonjun0902 <28wonjuny@students.tas.tw>
2026-05-02 19:40:00 +0800

    Finished elastic, still have some bugs tho. Gotta fix it later

--- src/main/java/frc/robot/lib/Elastic.java
+++ src/main/java/frc/robot/lib/Elastic.java
@@ -12,6 +12,7 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.PubSubOption;
 import edu.wpi.first.networktables.StringPublisher;
 import edu.wpi.first.networktables.StringTopic;
+import us.hebi.quickbuf.Descriptors.Descriptor;
 
 public class Elastic{
 
@@ -62,14 +63,14 @@ public class Elastic{
               @JsonProperty("level")
               private Notification level;
       
-              @JsonProperty("message")
-              private Notification message;
+              @JsonProperty("description")
+              private Notification description;
       
               @JsonProperty("title")
               private Notification title;
       
               @JsonProperty("displayTime")
-              private Notification displayTime;
+              private Notification displayTimeMillis;
       
               @JsonProperty("width")
               private Notification width;
@@ -101,12 +102,254 @@ public class Elastic{
         public Notification(NotificationLevel level, String title, String message, double displayTime, double width, double height) {
             this.level = level;
             this.title = title;
-            this.message = message;
-            this.displayTime = displayTime;
+            this.description = description;
+            this.displayTimeMillis = displayTimeMillis;
             this.width = width;
             this.height = height;
+      }
 
+      /**
+       * Creates a new Notification with the given parameters and default display time, width and height
+       * 
+       * @param level the {@link NotificationLevel} of the notification
+       * @param title the title of the notification  
+       * @param message the description of the notification
+       */
+      public Notification (NotificationLevel level, String title, String description) {
+        this(level, title, description, 5.0, 0.0, 0.0);
+      }
 
-    }
+      /**
+       * Updates the level of this notification 
+       * 
+       * @param level the level to set the notification to 
+       */
+      public void setLevel(NotificationLevel level) {
+        this.level = level;
+      }
+
+      /**
+       * @return the level of this notification
+       */
+      public NotificationLevel getLevel() {
+        return level;
+      }
+
+      /**
+       * Updates the title of this notification 
+       * 
+       * @param title the title to set the notification to 
+       */
+      public void setTitle(String title){
+        this.title = title;
+      }
+
+      /**
+       * Gets the title of this notification
+       * 
+       * @return the title of this notification 
+       */
+      public String getTitle(){
+        return title;
+      }
+
+      /**
+       * Updates the description of this notification 
+       * 
+       * @param message the description to set the notification to 
+       */
+      public void setDescription(String description){
+        this.description = description;
+      }
+
+      /**
+       * Gets the description of this notification
+       * 
+       * @return the description of this notification 
+       */
+      public String getDescription(){
+        return description;
+      }
+
+      /** 
+       * Updates the display time of this notification
+       * 
+       * @param seconds the display time in seconds to set the notification to.
+       */
+      public void setDisplayTimeSeconds(double seconds){
+        setDisplayTimeMillis((int) Math.round(seconds*1000));
+      }
+
+      /**
+       * Updates the display time of this notification
+       * 
+       * @param milliseconds the display time in milliseconds to set the notification to.
+       */
+      public void setDisplayTimeMillis(double milliseconds){
+        this.displayTimeMillis = displayTimeMillis;
+      }
+
+      /**
+       * Gets the display time of this notification in milliseconds 
+       * 
+       * @return the number of milliseconds that the notification is displayed for. 
+       */
+      public int getDisplayTimeMillis(){
+        return displayTimeMillis;
+      }
+
+      /**
+       * Updates the width of this notification 
+       * 
+       * @param width the width to set the notification for
+       */
+      public void setWidth(double width){
+        this.width = width;
+      }
+
+      public double getWidth(){
+        return width;
+      }
+
+      /**
+       * Updates the height of this notification 
+       * 
+       * <p> if the height is set to -1, the height will be determined automatically by the dashboard
+       * 
+       * @param height the height of the notification
+       */
+      public void setHeight(double height){
+        this.height = height;
+      }
+
+      /**
+       * Gets teh height of this notification 
+       * 
+       * @return height of the notification 
+       */
+      public double getHeight(){
+        return height;
+      }
+
+      /** 
+       * Modifies the notification's level and returns this notification for method chaining 
+       * 
+       * @param level the level to set the notification to 
+       * @return the current notification 
+       */
+      public Notification withLevel(NotificationLevel level){
+        this.level = level;
+        return this;
+      }
+
+      /** 
+       * Modifies the notification's title and returns this notification for method chaining 
+       * 
+       * @param title the title to set the notification to 
+       * @return the current notification 
+       */
+      public Notification withTitle(String title){
+        this.title = title;
+        return this;
+      }
+
+      /**
+       * Modifies the notification's description and returns this notification for method chaining
+       * 
+       * @param message the description to set the notification to
+       * @return the current notification
+       */
+      public Notification withDescription(String description){
+        this.description = description;
+        return this;
+      }
+
+      /**
+       * Modifies the notification's display time and returns itself to allow for method chaining
+       * 
+       * @param seconds the number of seconds to set the display time for
+       * @return the current notification
+       */
+      public Notification withDisplaySeoconds(double seconds){
+        setDisplayTimeSeconds(seconds);
+        return this;
+      }
+
+      /**
+       * Modifies the notification's display time and returns itself for method chaining
+       * 
+       * @param milliseconds the number of milliseconds to set the display time for
+       * @return the current notification
+       */
+      public Notification withDisplayMillis(double milliseconds){
+        setDisplayTimeMillis(milliseconds);
+        return this;
+      }
+      
+      /**
+       * Modifies the notification's width and returns itself for method chaining
+       * 
+       * @param width the width in pixels to set the notification to
+       * @return the current notification
+       */
+      public Notification withWidth(double width){
+        setWidth(width);
+        return this;
+      }
+
+      /**
+       * Modifies the notification's height and returns itself for method chaining
+       * 
+       * @param height the height in pixels to set the notification to. If this is set to -1, the height will be determined automatically by the dashboard
+       * @return the current notification
+       */
+      public Notification withHeight(double height){
+        setHeight(height);
+        return this;
+      }
+
+      /**
+       * Modifies the notification's height and returns itself for method chaining 
+       * 
+       * <p> This will set the height to -1 to have it automatically determined by the dashboard
+       * 
+       * @return the current notification
+       */
+      public Notification withAutoHeight(){
+        setHeight(-1);
+        return this;
+      }
+
+      /**
+       * Modifies the notification to disable the auto dismiss behavior 
+       * 
+       * <p> This sets the display time to 0 milliseconds 
+       * 
+       * <p> The auto dismiss behavior can be re-enabled by setting the display time to a number 
+       * greater than 0
+       * 
+       * THis is because the source code for the dashboard time measurements only considers numbers 
+       * greater than 0 to be valid display times, so setting the display time to 0 or less will 
+       * make the dashboard ignore the display time and not dimiss the display until the user 
+       * turns the dashbaord off. Lol ts coooool
+       */
+      public Notification withIndefiniteDisplay(){
+        setDisplayTimeMillis(0);
+        return this;
+      }
+
+      /**
+       * Represents the possible levels of a notification. 
+       * The level of a notification determines the color that it is displayed with on the dashboard, 
+       * with each level corresponding to a different color. 
+       * 
+       * The levels are INFO (blue), WARNING (yellow), and ERROR (red)
+       */
+      public enum NotificationLevel{
+        INFO,
+        WARNING,
+        ERROR
+      }
+
 }
 }
