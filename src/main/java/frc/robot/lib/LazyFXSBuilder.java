4f0d62e9911b1d0e13d593f00b5948e9163bde66
Wonjun0902 <28wonjuny@students.tas.tw>
2026-05-11 21:56:34 +0800

    Start LazyFXS Builder

--- src/main/java/frc/robot/lib/LazyFXSBuilder.java
+++ src/main/java/frc/robot/lib/LazyFXSBuilder.java
@@ -1,4 +1,29 @@
 package frc.robot.lib;
 
-public class LazyFXSBuilder{
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
+
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.ParentConfiguration;
+import com.ctre.phoenix6.configs.TalonFXSConfiguration;
+import com.ctre.phoenix6.hardware.TalonFXS;
+import com.ctre.phoenix6.signals.AdvancedHallSupportValue;
+import com.ctre.phoenix6.signals.ExternalFeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.signals.MotorArrangementValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
+
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.DriverStation;
+
+public class LazyFXSBuilder implements LazyCTREBuilder<TalonFXS, ExternalFeedbackSensorSourceValue> {
+    private MotorArrangementValue motorArrangementValue;
+    private int motorID, followID, canCoderID;
+    private String canBus = "", followCanbus = "", canCoderCanBus = "";
+
+    
 }
