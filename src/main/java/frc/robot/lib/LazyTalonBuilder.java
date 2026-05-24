c4dabee654322bd238dcf5568c3094bf71aa77e2
Wonjun0902 <28wonjuny@students.tas.tw>
2026-05-24 17:04:32 +0800

    Start LazyTalonBuilder.. Isn't this the same as LazyFXSBuilder....?

--- src/main/java/frc/robot/lib/LazyTalonBuilder.java
+++ src/main/java/frc/robot/lib/LazyTalonBuilder.java
@@ -1,4 +1,29 @@
 package frc.robot.lib;
 
-public class LazyTalonBuilder {
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecondPerSecond;
+
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.ParentConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+import com.ctre.phoenix6.signals.StaticFeedforwardSignValue;
+
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.DriverStation;
+
+public class LazyTalonBuilder implements LazyCTREBuilder<LazyTalon, FeedbackSensorSourceValue>{
+    private int motorID, folowID, canCoderID;
+    private String canBus = "", followCanbus = "" , canCoderCanBus = "";
+    private TalonFXConfiguration motorConfiguration = null, followConfiguration = null;
+    private CANcoderConfiguration canCoderConfiguration = null;
+
+    private MotorAlignmentValue followerInverted;
+
+    
 }
