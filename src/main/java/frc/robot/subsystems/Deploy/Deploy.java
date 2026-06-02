adf9228f157e88a6ac37b031c8b1f363aae05e6a
Wonjun0902 <28wonjuny@students.tas.tw>
2026-06-02 20:48:49 +0800

    Started Deploy subsystem

--- src/main/java/frc/robot/subsystems/Deploy/Deploy.java
+++ src/main/java/frc/robot/subsystems/Deploy/Deploy.java
@@ -1,5 +1,38 @@
 package frc.robot.subsystems.Deploy;
 
-public class Deploy {
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+import static frc.robot.subsystems.Deploy.DeployConstants.*;
+
+public class Deploy extends SubsystemBase{
+
+    private DeployIO io;
+    
+    public Deploy(DeployIO io) {
+        this.io = io;
+    }
+
+    public Command deploy(){
+        return run(() -> {
+            io.deploy();
+        });
+    }
+
+     public Command retract(){
+        return run(() -> {
+            io.retract();
+        });
+     }
 
 }
--- src/main/java/frc/robot/subsystems/Deploy/DeployIO.java
+++ src/main/java/frc/robot/subsystems/Deploy/DeployIO.java
@@ -2,4 +2,16 @@ package frc.robot.subsystems.Deploy;
 
 public interface DeployIO {
 
+    public void deploy();
+    public void retract();
+    public void stop();
+
+    public void periodic();
+
+    public void getSpeed();
+
+    public void getCurrent();
+    
+    public void getPosition();
+
 }
