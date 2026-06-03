381719dec00f31c6c997da8361fe9243a4e8aca1
Wonjun0902 <28wonjuny@students.tas.tw>
2026-06-03 12:06:33 +0800

    Write new logic for intake deploy

--- src/main/java/frc/robot/subsystems/Deploy/Deploy.java
+++ src/main/java/frc/robot/subsystems/Deploy/Deploy.java
@@ -1,7 +1,9 @@
 package frc.robot.subsystems.Deploy;
 
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Time;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -15,6 +17,7 @@ import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 import static edu.wpi.first.units.Units.Seconds;
 import static frc.robot.subsystems.Deploy.DeployConstants.*;
+import static frc.robot.subsystems.Deploy.DeployConstants.Configurations.Positions.*;
 
 public class Deploy extends SubsystemBase{
 
@@ -31,23 +34,39 @@ public class Deploy extends SubsystemBase{
 
         isAtM1 = new Trigger(() -> io.getPosition().isNear(M1_ANGLE, DEPLOY_TOLERANCE));
         isAtM2 = new Trigger(() -> io.getPosition().isNear(M2_ANGLE, DEPLOY_TOLERANCE));
+        
         isRetracted = new Trigger(() -> io.getPosition().isNear(RETRACTED_ANGLE, DEPLOY_TOLERANCE));
         isDeployed = new Trigger(() -> io.getPosition().isNear(DEPLOYED_ANGLE, DEPLOY_TOLERANCE));
     }
 
-    public AngularVelocity getSpeed(){
-        return DEPOLY_SPEED;
+    public Command Deploy(){
+        return runOnce(() -> io.deploy(M1_ANGLE));
     }
 
-    public Command deploy(AngularVelocity speed){
-        return run(() -> {
-            io.deploy(getSpeed());
-        });
+    public Command Retract(){
+        return runOnce(() -> io.retract(RETRACTED_ANGLE));
     }
 
-     public Command retract(AngularVelocity speed){
-        return run(() -> {
-            io.retract(getSpeed().unaryMinus());
-        });
-     }
+    public Command Agitate(Angle StartPoint, Angle EndPoint, Time duration){
+        return new SequentialCommandGroup(
+            run(() -> io.agitateFrom(StartPoint, EndPoint, duration)).withTimeout(duration.in(Seconds)),
+            run(() -> io.agitateFrom(EndPoint, StartPoint, duration)).withTimeout(duration.in(Seconds))
+        );
+    }
+
+    public Command AgitateM1(){
+        return Agitate(DEPLOYED_ANGLE, M1_ANGLE, Seconds.of(0.5)); //Time Placeholder
+    }
+
+    public Command AgitateM2(){
+        return Agitate(DEPLOYED_ANGLE, M2_ANGLE, Seconds.of(0.5)); //Time Placeholder
+    }
+
+    public Command FullAgitate(){
+        return Agitate(DEPLOYED_ANGLE, RETRACTED_ANGLE, Seconds.of(0.5)); //Time Placeholder
+    }
+
+    public Command MidAgitate(){
+        return Agitate(M1_ANGLE, M2_ANGLE, Seconds.of(0.5)); //Time Placeholder
+    }
 }
--- src/main/java/frc/robot/subsystems/Deploy/DeployIO.java
+++ src/main/java/frc/robot/subsystems/Deploy/DeployIO.java
@@ -1,13 +1,20 @@
 package frc.robot.subsystems.Deploy;
 
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Time;
 
 public interface DeployIO {
 
-    public void deploy(AngularVelocity speed);
-    public void retract(AngularVelocity speed);
+    public void deploy(Angle position);
+    public void retract(Angle position);
+
+    public void moveToM1(Angle position);
+    public void moveToM2(Angle position);
+
+    public void agitateFrom(Angle StartPoint, Angle EndPoint, Time duration);
 
     public void runDutyCycleLeft(double dutyCycle);
     public void runDutyCycleRight(double dutyCycle);
@@ -20,5 +27,5 @@ public interface DeployIO {
 
     public void getCurrent();
     
-    public void getPosition();
+    public Angle getPosition();
 }
