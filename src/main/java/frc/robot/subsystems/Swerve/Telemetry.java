aea29e041e433f9beb2564f47d3ef5ee212bfe4c
Wonjun0902 <28wonjuny@students.tas.tw>
2026-06-09 19:21:08 +0800

    Start Swerve Telemetry Subsystem

--- src/main/java/frc/robot/subsystems/Swerve/Telemetry.java
+++ src/main/java/frc/robot/subsystems/Swerve/Telemetry.java
@@ -21,4 +21,48 @@ import edu.wpi.first.wpilibj.util.Color8Bit;
 
 public class Telemetry {
 
+    public final double MaxSpeed;
+
+    /**
+     * Constructs a telemetry object, with a specified max speed of the bot
+     * 
+     * @param maxSpeed Maximum speed of the bot in meters per second
+     * */ 
+    public Telemetry(double maxSpeed){
+        MaxSpeed = maxSpeed;
+        SignalLogger.start();
+
+        for(int i = 0; i < 4; ++i){
+            SmartDashboard.putData("Module"+i, m_moduleMechanism[i]);
+        }
+    }
+
+    //What to publish over network tables for telemetry 
+    private final NetworkTableInstance inst = NetworkTableInstance.getDefault();
+
+    //Robot Swerve DriveState
+    private final NetworkTable driveStateTable = inst.getTable("DriveState Table");
+    private final StructPublisher<Pose2d> drivePose = driveStateTable.getStructTopic("Pose", Pose2d.struct).publish();
+    private final StructPublisher<ChassisSpeeds> driveSpeeds = driveStateTable.getStructTopic("Speeds", ChassisSpeeds.struct).publish();
+    private final StructArrayPublisher<SwerveModuleState> driveModuleStates = driveStateTable
+        .getStructArrayTopic("Module State", SwerveModuleState.struct).publish();
+    private final StructArrayPublisher<SwerveModuleState> driveModuleTargets = driveStateTable
+        .getStructArrayTopic("Module Target", SwerveModuleState.struct).publish();
+    private final StructArrayPublisher<SwerveModulePosition> driveModulePositions = driveStateTable
+        .getStructArrayTopic("Swerve Positions", SwerveModulePosition.struct).publish();
+    private final DoublePublisher driveTimeStamp = driveStateTable.getDoubleTopic("Time Stamp").publish();
+    private final DoublePublisher driveOdometryFrequency = driveStateTable.getDoubleTopic("Odometry Frequency").publish();
+
+    //Robot Pose for field positioning 
+    private final NetworkTable table = inst.getTable("Pose");
+    private final DoubleArrayPublisher fieldPub = table.getDoubleArrayTopic("Robot Pose").publish();
+    private final StringPublisher fieldTypePub = table.getStringTopic(".Type").publish();
+
+    //Mechanisms to represent the swerve module states 
+    private final Mechanism2d[] m_moduleMechanism = new Mechanism2d[]{
+        new Mechanism2d(1, 1),
+        new Mechanism2d(1,1),
+        new Mechanism2d(1, 1),
+        new Mechanism2d(1,1),
+    };
 }
