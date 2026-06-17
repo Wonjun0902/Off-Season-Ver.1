32e541c6d083c215dfecf6dbe6adccbbe693d595
Wonjun0902 <28wonjuny@students.tas.tw>
2026-06-17 15:57:20 +0800

    Start AutoAlign Logic - not my logic but the made one

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,4 +2,24 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-// Later Lol
+import java.util.Optional;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import frc.robot.subsystems.Swerve.Swerve;
+import frc.robot.subsystems.Swerve.TunerConstants;
+
+public class RobotContainer {
+
+    public static final Swerve drivetrain = TunerConstants.createDrivetrain();
+
+    private static Optional<Alliance> alliance = null;
+
+    Optional<Alliance> alliance = DriverStation.getAlliance();
+    Boolean blue = alliance.get().equals(Alliance.Blue);
+
+    public static Optional<Alliance> getCachedAlliance() {
+    if (alliance == null) alliance = DriverStation.getAlliance();
+    return alliance;
+  }
+}
--- src/main/java/frc/robot/subsystems/Shooter/AutoAlign.java
+++ src/main/java/frc/robot/subsystems/Shooter/AutoAlign.java
@@ -1,5 +1,30 @@
 package frc.robot.subsystems.Shooter;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Radians;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.DriverControls;
+import frc.robot.subsystems.Swerve.TunerConstants;
+
+import static frc.robot.RobotContainer.drivetrain;
+import static frc.robot.RobotContainer.getCachedAlliance;
+
+import java.util.function.Supplier;
+
+
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.ctre.phoenix6.swerve.SwerveRequest.ForwardPerspectiveValue;
+
+/**
+ * A class that manages the bot's autoalign logic for shooting at any position
+ * */ 
 public class AutoAlign {
 
 }
