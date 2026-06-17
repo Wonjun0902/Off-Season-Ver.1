479c5f03f77433989e32e1824cf9dac12ae04ce3
Wonjun0902 <28wonjuny@students.tas.tw>
2026-06-17 18:47:23 +0800

    AutoAlgin Logic - Half Way done

--- src/main/java/frc/robot/subsystems/Shooter/AutoAlign.java
+++ src/main/java/frc/robot/subsystems/Shooter/AutoAlign.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems.Shooter;
 
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.Rotation;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -12,19 +13,49 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.DriverControls;
 import frc.robot.subsystems.Swerve.TunerConstants;
-
 import static frc.robot.RobotContainer.drivetrain;
 import static frc.robot.RobotContainer.getCachedAlliance;
-
 import java.util.function.Supplier;
-
-
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.ctre.phoenix6.swerve.SwerveRequest.ForwardPerspectiveValue;
+import static frc.robot.subsystems.Shooter.ShooterConstants.AngleLockConfig.*;
+
+import frc.robot.DriverControls;
 
 /**
  * A class that manages the bot's autoalign logic for shooting at any position
- * */ 
+ */ 
 public class AutoAlign {
 
+    //Creates a new Swerve Request for AngleLock with PID values
+    private static final SwerveRequest.FieldCentricFacingAngle angleLock = new SwerveRequest.FieldCentricFacingAngle()
+        .withHeadingPID(ANGLELOCK_P, ANGLELOCK_I, ANGLELOCK_D) // Needs to be Tuned
+        .withMaxAbsRotationalRate(MAX_ROT_ANGLELOCK)// Needs to be Tuned
+    
+    //Creates Allign Command 
+    public Command align(Angle setPoint){
+        //Creates Target
+        Rotation2d target = new Rotation2d(setPoint);
+
+        return drivetrain.applyRequest(() -> 
+            angleLock
+                .withTargetDirection(target)
+                .withForwardPerspective(ForwardPerspectiveValue.OperatorPerspective)
+                .withVelocityX(0)
+                .withVelocityY(0)
+        )
+        .beforeStarting(() -> angleLock.HeadingController.reset());
+    }
+
+    public Command alignForPassing(){
+        //Pass Angle = 180 degrees from your perpective     
+        Rotation2d PassAngle = new Rotation2d(Math.PI);
+
+        return drivetrain.applyRequest(
+            angleLock
+                .withTargetDirection(PassAngle)
+                .withForwardPerspective(ForwardPerspectiveValue.OperatorPerspective)
+                .withVelocityX(DriverControls.defaultDriveControls.)
+        )
+    }
 }
--- src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterConstants.java
@@ -1,5 +1,28 @@
 package frc.robot.subsystems.Shooter;
 
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularAcceleration;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Time;
+
 public class ShooterConstants {
+
+    public class AngleLockConfig{
+        public static final AngularVelocity MAX_ROT_ANGLELOCK = RotationsPerSecond.of(0.0);
+
+        public static final Angle ROT_ANGLE_TOLERANCE = Rotations.of(0.0);
+
+        public static final double ANGLELOCK_P = 0.0;
+        public static final double ANGLELOCK_I = 0.0;
+        public static final double ANGLELOCK_D = 0.0;
+    }
 
 }
