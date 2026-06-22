86d8cf0c3a0163951fc12162a5a64d6f858984da
Wonjun0902 <28wonjuny@students.tas.tw>
2026-06-22 13:42:13 +0800

    ThroatIO add

--- src/main/java/frc/robot/subsystems/Throat/ThroatIO.java
+++ src/main/java/frc/robot/subsystems/Throat/ThroatIO.java
@@ -1,5 +1,16 @@
 package frc.robot.subsystems.Throat;
 
+import edu.wpi.first.units.measure.AngularVelocity;
+
 public interface ThroatIO {
+
+    public void setThroatSpeed(AngularVelocity velocity);
+    
+    public void stop();
+
+    public AngularVelocity getRightSpeed();
+    public AngularVelocity getLeftSpeed();
+
+    public void periodic();
 
 }
