f2c741f204433d8023d2685536241fe46ac1c3e1
Wonjun0902 <28wonjuny@students.tas.tw>
2026-06-22 18:27:43 +0800

    add Deployer Sim

--- src/main/java/frc/robot/subsystems/Deploy/DeployIOSim.java
+++ src/main/java/frc/robot/subsystems/Deploy/DeployIOSim.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems.Deploy;
 
-public class DeployIOSim {
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+public class DeployIOSim extends SubsystemBase implements DeployIO{
+
+    private DCMotor gearbox;
+    private DCMotorSim motorSim;
+
+    public DeployIOSim(){
+        gearbox = DCMotor.getKrakenX44(2);
+        motorSim = new DCMotorSim(LinearSystemId.createDCMotorSystem(gearbox, 0.1,1 ), gearbox);
+    }
+
+    @Override
+    public void moveTo(Angle position){
+        motorSim.setInputVoltage(0);
+    }
 }
