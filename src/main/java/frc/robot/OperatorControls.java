0d36c159c46e71b2bce109b2d96b7e3d164efe5a
Wonjun0902 <28wonjuny@students.tas.tw>
2026-06-23 21:49:32 +0800

    Finish Operator Controls - only intaking

--- src/main/java/frc/robot/OperatorControls.java
+++ src/main/java/frc/robot/OperatorControls.java
@@ -1,5 +1,19 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+import static frc.robot.RobotContainer.indexer;
+import static frc.robot.RobotContainer.intake;
+import static frc.robot.RobotContainer.mrPatel;
+import static frc.robot.RobotContainer.shooter;
+import static frc.robot.RobotContainer.throat;
+
 public class OperatorControls {
 
+    public static CommandXboxController OPERATOR_CONTROLLER = new CommandXboxController(1);
+
+    public static void ConfigureBindings(){
+        OPERATOR_CONTROLLER.a().onTrue(mrPatel.beginIntaking());
+    }
 }
